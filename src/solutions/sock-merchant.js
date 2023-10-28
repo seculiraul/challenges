b57a7a2db40fb391a229e17c2e864350b57fb0f5
Raul <raulsmarius@yahoo.com>
2023-10-28 16:45:26 +0300

    sock-merchant Solution

---
+++ src/solutions/sock-merchant.js
@@ -0,0 +1,40 @@
+// There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// Example
+
+// There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+// Function Description
+
+// Complete the sockMerchant function in the editor below.
+
+// sockMerchant has the following parameter(s):
+
+// int n: the number of socks in the pile
+// int ar[n]: the colors of each sock
+// Returns
+
+// int: the number of pairs
+// Input Format
+
+// The first line contains an integer , the number of socks represented in .
+// The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+// Sample Input
+
+// STDIN                       Function
+// -----                       --------
+// 9                           n = 9
+// 10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+// Sample Output
+
+// 3
+
+// SOLUTION
+
+function sockMerchant(n, ar) {
+  return [...new Set(ar)].reduce((acc, sock) => (acc += pairs(ar, sock)), 0)
+}
+
+const pairs = (arr, want) =>
+  Math.trunc(arr.reduce((acc, el) => (el === want ? acc + 1 : acc), 0) / 2)
