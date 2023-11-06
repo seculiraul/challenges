2f0aa67a323b6c2e913ca20b05dca882452a233a
Raul <raulsmarius@yahoo.com>
2023-11-06 21:31:10 +0200

    strange-advertising Solution

---
+++ src/solutions/strange-advertising.js
@@ -0,0 +1,44 @@
+// HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  people on social media.
+
+// On the first day, half of those  people (i.e., ) like the advertisement and each shares it with  of their friends. At the beginning of the second day,  people receive the advertisement.
+
+// Each day,  of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day .
+
+// Example
+// .
+
+// Day Shared Liked Cumulative
+// 1      5     2       2
+// 2      6     3       5
+// 3      9     4       9
+// 4     12     6      15
+// 5     18     9      24
+// The progression is shown above. The cumulative number of likes on the  day is .
+
+// Function Description
+
+// Complete the viralAdvertising function in the editor below.
+
+// viralAdvertising has the following parameter(s):
+
+// int n: the day number to report
+// Returns
+
+// int: the cumulative likes at that day
+// Input Format
+
+// A single integer, , the day number.
+
+// SOLUTION
+
+function viralAdvertising(n) {
+  let totalLikes = 0
+  let nrOfPeople = 5
+
+  for (let i = 0; i < n; i++) {
+    const peopleLikes = Math.floor(nrOfPeople / 2)
+    totalLikes = totalLikes + peopleLikes
+    nrOfPeople = peopleLikes * 3
+  }
+  return totalLikes
+}
